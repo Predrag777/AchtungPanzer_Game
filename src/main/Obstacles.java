302b77d48f9b81f1d66fb3ee4b467d16fbe85d06
Predrag777 <predragbrajovicSS@gmail.com>
2024-11-11 18:06:58 +0100

    Map features

--- src/main/Obstacles.java
+++ src/main/Obstacles.java
@@ -3,12 +3,15 @@ package main;
 public class Obstacles {
 	String name;
 	int x,y;
+	int width,height;
 	boolean destroyable;
 	
-	public Obstacles(String name, int x, int y, boolean destroyable) {
+	public Obstacles(String name, int x, int y,int width,int height, boolean destroyable) {
 		this.name=name;
 		this.x=x;
 		this.y=y;
 		this.destroyable=destroyable;
+		this.width=width;
+		this.height=height;
 	}
 }
---
+++ src/main/Trench.java
@@ -0,0 +1,10 @@
+package main;
+
+public class Trench extends Obstacles{
+
+	public Trench(String name, int x, int y, int width, int height, boolean destroyable) {
+		super(name, x, y, width, height, destroyable);
+		// TODO Auto-generated constructor stub
+	}
+
+}
